5276c1c806608e705e8801068518ab4c7ed016d7
Aldair Gonzalez Conde <aldair.gonzalez@romio.mx>
2025-02-26 23:29:16 -0600

    Corrige rutas para despliegue en Render program

--- Backend/Configuration/ConfigureMiddleware.cs
+++ Backend/Configuration/ConfigureMiddleware.cs
@@ -3,11 +3,8 @@ public static class ConfigureMiddleware
 {
     public static void Configure(WebApplication app)
     {
-        if (app.Environment.IsDevelopment())
-        {
-            app.UseSwagger();
-            app.UseSwaggerUI();
-        }
+        app.UseSwagger();
+        app.UseSwaggerUI();
 
         app.UseCors(policy =>
             policy.SetIsOriginAllowed(_ => true)
